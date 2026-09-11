f2ec0856b0433bc104a9df291f85cb0b8e1dc063
rei1192 <reo1206reo1206@gmail.com>
2026-09-12 04:41:10 +0900

    Upgrade global navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import Link from 'next/link'
 
 export const metadata = {
-  title: 'TripNest',
-  description: '旅をもっと自由に楽しむ宿泊アプリ',
+  title: 'TripNest | 旅をもっと自由に',
+  description: '全国の宿を探して、保存して、コインで予約できるトラベルメンバーシップ',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -13,13 +13,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="appShell">
           <header className="topbar">
             <Link href="/" className="brand">Trip<span>Nest</span></Link>
-            <Link href="/account" className="accountLink">マイページ</Link>
+            <div className="topActions"><Link href="/reservations" className="ghostLink">予約</Link><Link href="/account" className="accountLink">マイページ</Link></div>
           </header>
           {children}
           <nav className="bottomNav">
             <Link href="/">⌂<span>ホーム</span></Link>
             <Link href="/#search">⌕<span>探す</span></Link>
-            <Link href="/#favorites">♡<span>お気に入り</span></Link>
+            <Link href="/#favorites">♡<span>保存</span></Link>
+            <Link href="/reservations">▣<span>予約</span></Link>
             <Link href="/account">◎<span>アカウント</span></Link>
           </nav>
         </div>
