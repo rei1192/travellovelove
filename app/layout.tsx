ea1614b62569be4dbd0ed01b5a91624c7c4cbca4
rei1192 <reo1206reo1206@gmail.com>
2026-09-12 04:47:55 +0900

    Upgrade navigation for map and saved pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export const metadata = {
   title: 'TripNest | 旅をもっと自由に',
-  description: '全国の宿を探して、保存して、コインで予約できるトラベルメンバーシップ',
+  description: '実在ホテルを検索・保存・地図・予約までひとつにまとめたトラベルアプリ',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -13,15 +13,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="appShell">
           <header className="topbar">
             <Link href="/" className="brand">Trip<span>Nest</span></Link>
-            <div className="topActions"><Link href="/reservations" className="ghostLink">予約</Link><Link href="/account" className="accountLink">マイページ</Link></div>
+            <div className="topActions"><Link href="/map" className="ghostLink">地図</Link><Link href="/account" className="accountLink">マイページ</Link></div>
           </header>
           {children}
-          <nav className="bottomNav">
-            <Link href="/">⌂<span>ホーム</span></Link>
-            <Link href="/#search">⌕<span>探す</span></Link>
-            <Link href="/#favorites">♡<span>保存</span></Link>
-            <Link href="/reservations">▣<span>予約</span></Link>
-            <Link href="/account">◎<span>アカウント</span></Link>
+          <nav className="bottomNav premiumBottomNav">
+            <Link href="/"><b>⌂</b><span>ホーム</span></Link>
+            <Link href="/map"><b>◉</b><span>マップ</span></Link>
+            <Link href="/saved"><b>♡</b><span>保存</span></Link>
+            <Link href="/reservations"><b>▣</b><span>予約</span></Link>
+            <Link href="/account"><b>◎</b><span>アカウント</span></Link>
           </nav>
         </div>
       </body>
